c675652b47b8f209553243e005b61e72f2e3d833
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-05-09 17:54:50 +0200

    Create data.js Character.js and utils.js and separate code neatly.Create a single const for both characters in data.js. Refactor functions in index.js to match the above changes.

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
-const hero = {
-  elementId: 'hero',
-  name: 'Wizard',
-  avatar: 'images/wizard.png',
-  health: 60,
-  diceCount: 3,
-};
+import Character from '/Character.js';
+import characterData from '/data.js';
 
-const monster = {
-  elementId: 'monster',
-  name: 'Orc',
-  avatar: 'images/orc.png',
-  health: 10,
-  diceCount: 1,
-};
-
-function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function () {
-    return Math.floor(Math.random() * 6) + 1;
-  });
-}
-
-//constructor function serves as template to create multiple objects with similar attributes. Object.Assign(target, source) makes it so that I do not need to write this.property line after line).
-function Character(data) {
-  Object.assign(this, data);
-  this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount } = this;
-    let diceHtml = this.getDiceHtml(diceCount);
-
-    return `
-        <div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}" />
-            <div class="health">health: <b> ${health} </b></div>
-            <div class="dice-container">
-               ${diceHtml}
-            </div>
-        </div>`;
-  };
-  this.getDiceHtml = function (diceCount) {
-    return getDiceRollArray(diceCount)
-      .map(function (num) {
-        return `<div class="dice">${num}</div>`;
-      })
-      .join('');
-  };
-}
-
-const wizard = new Character(hero);
-const orc = new Character(monster);
 function render() {
   document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
   document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
+
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 render();
