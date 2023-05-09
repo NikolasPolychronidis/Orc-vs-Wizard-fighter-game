e28e11f87902be1c3e3a7ee6de2a15b284d7f8a4
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-05-09 14:28:58 +0200

    Refactor the GetDiceRollArray function and replace the for loop with array constructor fill and map methods

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ const monster = {
 };
 
 function getDiceRollArray(diceCount) {
-  let newDiceRolls = [];
-  for (let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return newDiceRolls;
+  return new Array(diceCount).fill(0).map(function () {
+    return Math.floor(Math.random() * 6) + 1;
+  });
 }
 
 function getDiceHtml(diceCount) {
