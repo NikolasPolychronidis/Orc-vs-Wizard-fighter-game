5acd1309bb6910b73ce528f9566925c458ddd808
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-05-10 16:17:10 +0200

    Set up health percentage functionality and healthbar

--- data.js
+++ data.js
@@ -2,15 +2,15 @@ const characterData = {
   hero: {
     name: 'Wizard',
     avatar: 'images/wizard.png',
-    health: 60,
+    health: 100,
     diceCount: 3,
     currentDiceScore: [],
   },
   monster: {
     name: 'Orc',
     avatar: 'images/orc.png',
-    health: 10,
-    diceCount: 1,
+    health: 100,
+    diceCount: 3,
     currentDiceScore: [],
   },
 };
