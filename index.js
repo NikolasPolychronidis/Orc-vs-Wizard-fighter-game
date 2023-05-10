ebf0baff139a1a539bb7f2b9140e8c3ac05c5e4d
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-05-10 14:04:41 +0200

    Enable functionality to reduce health of characters when damage is taken. Set up the endGame functionality and display end message when one of the two characters is dead.

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import Character from '/Character.js';
 import characterData from '/data.js';
 
+function endGame() {
+  const endMessage =
+    wizard.health === 0 && orc.health === 0
+      ? 'No one wins, both are dead'
+      : wizard.health > 0
+      ? 'Wizard wins'
+      : 'Orc wins';
+
+  const endEmoji = wizard.health > 0 ? '🔮' : '☠️';
+  document.body.innerHTML = `<div class="end-game">
+              <h2>Game Over</h2>
+              <h3>${endMessage}</h3>
+              <p class="end-emoji">${endEmoji}</p>
+          </div>`;
+}
+
 function attack() {
   wizard.getDiceHtml();
   orc.getDiceHtml();
   wizard.takeDamage(orc.currentDiceScore);
   orc.takeDamage(wizard.currentDiceScore);
   render();
+  if (orc.dead || wizard.dead) {
+    endGame();
+  }
 }
 
 function render() {
